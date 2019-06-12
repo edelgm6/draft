07a5c236f8304b0d0f79ca514400c0e4f56b4452
Garrett Edel <edelgm6@gmail.com>
2019-06-11 18:54:23 -0700

    Removed coverage from setup.py (#55)

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click',
-        'mistune'
+        'Click'
     ],
     entry_points='''
         [console_scripts]
